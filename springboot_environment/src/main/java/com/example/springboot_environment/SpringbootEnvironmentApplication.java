c70c4a85a9fba19f9d3f65eeab8cf598d840576a
xuhongyu <xuhongyu@yazuishou.com>
2022-06-13 15:41:42 +0800

    项目初始化

---
+++ springboot_environment/src/main/java/com/example/springboot_environment/SpringbootEnvironmentApplication.java
@@ -0,0 +1,16 @@
+package com.example.springboot_environment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xiayu
+ */
+@SpringBootApplication
+public class SpringbootEnvironmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootEnvironmentApplication.class, args);
+    }
+
+}
